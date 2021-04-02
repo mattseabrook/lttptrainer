a545efdea956b4b45a8c0d7ff1b316dd441311bc
Matthew Seabrook <info@mattseabrook.net>
2021-04-02 18:42:58 -0500

    Updates & Additions

---
+++ src/snes9x_hook.h
@@ -0,0 +1,17 @@
+// snes9x_hook.h
+
+#ifndef SNES9X_HOOK_H
+#define SNES9X_HOOK_H
+
+// definitions...
+
+//===========================================================================
+
+void something();
+
+//===========================================================================
+
+//extern char snes_nine_x_path[1000];
+//extern int sramtrace_interval;
+
+#endif
